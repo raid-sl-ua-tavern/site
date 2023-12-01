e6ec8ef72fbdd26a59975a0c96d3ce3865a6ef2c
raid-sl-ua-tavern <151370065+raid-sl-ua-tavern@users.noreply.github.com>
2023-12-01 22:17:52 +0200

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -7,34 +7,34 @@ let linkTag = searchWrapper.querySelector("a");
 let webLink;
 
 // if user press any key and release
-inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
-            webLink = `#`;
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
-        }
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
-}
+// inputBox.onkeyup = (e)=>{
+//     let userData = e.target.value; //user enetered data
+//     let emptyArray = [];
+//     if(userData){
+//         icon.onclick = ()=>{
+//             webLink = `#`;
+//             linkTag.setAttribute("href", webLink);
+//             linkTag.click();
+//         }
+//         emptyArray = suggestions.filter((data)=>{
+//             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+//             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+//         });
+//         emptyArray = emptyArray.map((data)=>{
+//             // passing return data inside li tag
+//             return data = `<li>${data}</li>`;
+//         });
+//         searchWrapper.classList.add("active"); //show autocomplete box
+//         showSuggestions(emptyArray);
+//         let allList = suggBox.querySelectorAll("li");
+//         for (let i = 0; i < allList.length; i++) {
+//             //adding onclick attribute in all li tag
+//             allList[i].setAttribute("onclick", "select(this)");
+//         }
+//     }else{
+//         searchWrapper.classList.remove("active"); //hide autocomplete box
+//     }
+// }
 
 function select(element){
     let selectData_default = element.textContent;
